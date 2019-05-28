8fe468effc5f4738c7f1c507a2195409bb4d79c3
xdyangwei <xdgodyw@gmail.com>
2019-05-28 14:15:02 +0800

    complete isSameTree problem

--- leetcode_medium_54.cpp
+++ leetcode_medium_54.cpp
@@ -934,6 +934,20 @@ vector<int> grayCode(int n) {
     return res;
 }
 
+struct TreeNode {
+         int val;
+         TreeNode *left;
+         TreeNode *right;
+         TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+     };
+bool isSameTree(TreeNode* p, TreeNode* q) {
+    if(p== nullptr&&q== nullptr)
+        return true;
+    if(p== nullptr||q== nullptr||p->val!=q->val)
+        return false;
+    return isSameTree(p->left,q->left)&&isSameTree(p->right,q->right);
+}
+
 int main(){
 
 
