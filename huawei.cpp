addc81427d94171d32797d5d69671e8f310cf125
xdyangwei <xdgodyw@gmail.com>
2019-07-12 21:39:49 +0800

    complete the str_intercept problem

--- huawei.cpp
+++ huawei.cpp
@@ -632,6 +632,32 @@ void number_of_one_binary(){
     }
 }
 
+//编写一个截取字符串的函数，输入为一个字符串和字节数，输出为按字节截取的字符串。
+//但是要保证汉字不被截半个，如"我ABC"4，应该截为"我AB"，输入"我ABC汉DEF"6，
+//应该输出为"我ABC"而不是"我ABC+汉的半个"。
+//使用string判断每个字符
+void str_intercept(){
+    string s;int num;
+    while(cin>>s>>num){
+        string str="";
+        for(int i=0;i<s.size()&&num>=1;){
+            if((s[i]>=65&&s[i]<=90)||(s[i]>=97&&s[i]<=122)){
+                str+=s[i];num-=1;i++;
+            }
+            else{
+                if(num<=1)
+                    break;
+                else
+                {
+                    cout<<s[i]<<endl;
+                    str+=(string(s.begin()+i,s.begin()+1));num-=2;i+=1;
+                }
+            }
+        }
+        cout<<str<<endl;
+    }
+}
+
 int main(){
     triangle_sum();
     return 0;
